25f35b5872a2b0c4b3998e22982b425b0a72c0b4
황종훈 <ybell1028@gmail.com>
2021-03-31 20:54:44 +0900

    Chore : 스프링 부트 프로젝트 기본 세팅

---
+++ src/main/java/com/jh/itcl/ItclApplication.java
@@ -0,0 +1,11 @@
+package com.jh.itcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItclApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItclApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jh/itcl/ItclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.itcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
