2be548ea26f24588f7370d42bdc7a042acc52e29
황종훈 <ybell1028@gmail.com>
2021-04-08 10:58:59 +0900

    Add : Post Repository, ApiController 테스트 코드 작성

---
+++ src/test/java/com/jh/itcl/domain/post/PostRepositoryTest.java
@@ -0,0 +1,94 @@
+package com.jh.itcl.domain.post;
+
+import com.jh.itcl.domain.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+// 별 다른 설정 없이 @SpringBootTest를 사용할 경우 H2 데이터베이스 사용
+@SpringBootTest
+public class PostRepositoryTest {
+    @Autowired
+    PostRepository postRepository;
+
+    //    @AfterEach // JUnit에서 단위 테스트가 끝날 때마다 수행되는 메소드
+//    public void cleanup() {
+//        postRepository.deleteAll();
+//    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        User user = new User("ybell1028", "google", "황종훈", "ybell1028@gmail.com", null, null);
+
+        String title = "테스트 게시글";
+        String category = "개발";
+        String contents = "테스트 본문";
+        Integer view = 0;
+        Integer love = 0;
+
+        //given
+        postRepository.save(Post.builder()
+                .user(user)
+                .category(category)
+                .title(title)
+                .contents(contents)
+                .view(view)
+                .love(love)
+                .build()
+        );
+
+        //when
+        List<Post> postList = postRepository.findAll();
+
+        //then - 예상값과 동일한지 확인
+        Post post = postList.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getCategory()).isEqualTo(category);
+        assertThat(post.getContents()).isEqualTo(contents);
+        assertThat(post.getView()).isEqualTo(view);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        User user = new User("ybell1028", "google", "황종훈", "ybell1028@gmail.com", null, null);
+
+        String title = "테스트 게시글";
+        String category = "개발";
+        String contents = "테스트 본문";
+        Integer view = 0;
+        Integer love = 0;
+
+        //given
+        postRepository.save(Post.builder()
+                .user(user)
+                .category(category)
+                .title(title)
+                .contents(contents)
+                .view(view)
+                .love(love)
+                .build()
+        );
+
+        //when
+        List<Post> postsList = postRepository.findAll();
+
+        //then
+        Post post = postsList.get(0);
+
+        System.out.println(">>>>>>>> created_at = " + post.getCreated_at()
+                + ", modified_at = " + post.getModified_at());
+
+        assertThat(post.getCreated_at()).isAfter(now);
+        assertThat(post.getModified_at()).isAfter(now);
+    }
+}
---
+++ src/test/java/com/jh/itcl/web/PostApiControllerTest.java
@@ -0,0 +1,77 @@
+package com.jh.itcl.web;
+
+import com.jh.itcl.domain.post.Post;
+import com.jh.itcl.domain.post.PostRepository;
+import com.jh.itcl.domain.user.User;
+import com.jh.itcl.web.dto.PostSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception{
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void Post_등록된다() throws Exception {
+        // given
+        // 지금은 User 객체를 생성하고 있지만 후에는 access 토큰으로 정보를 받아보아야하나?
+        // 아니면 access 토큰으로 만드는 객체를 Bean으로 만들어서 Autowired로 의존성을 주입해줘야 하나?
+        User user = new User("ybell1028", "google", "황종훈", "ybell1028@gmail.com", null, null);
+
+        String title = "테스트 게시글";
+        String category = "개발";
+        String contents = "테스트 본문";
+        Integer view = 0;
+        Integer love = 0;
+
+        PostSaveRequestDto requestDto = PostSaveRequestDto.builder()
+                .user(user)
+                .category(category)
+                .title(title)
+                .contents(contents)
+                .view(view)
+                .love(love)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/post";
+
+        //when - url로 만들어 둔 requestDto를 보낸다.
+        //mvc.perform - 생성된 MockMvc를 통해 API를 테스트한다.
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Post> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getCategory()).isEqualTo(category);
+        assertThat(all.get(0).getContents()).isEqualTo(contents);
+        assertThat(all.get(0).getView()).isEqualTo(view);
+        assertThat(all.get(0).getLove()).isEqualTo(love);
+    }
+}
